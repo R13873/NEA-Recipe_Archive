cd71b2711add494b0222151fe155c42ea72a9ce0
R13873 <R13873@qmc.ac.uk>
2026-01-16 12:54:43 +0000

    deleting redundant stuff

--- display1.sql
+++
@@ -1,6 +0,0 @@
-SELECT Meals.meal_name as Name, Meals.servings as Serves, Ingredients.ingred_name as Ingredient, Recipes.amount as Amount, Units.unit_value as Unit, 0 as Price_per_Serving
-FROM Recipes, Meals, Ingredients, Units
-WHERE Recipes.meal_id = Meals.meal_id
-AND Recipes.ingred_id = Ingredients.ingred_id
-AND Recipes.unit_id = Units.unit_id
-ORDER BY Meals.meal_id
